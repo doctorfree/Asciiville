d5eca4e6ab89fab12681d8d3d56f56b83fb1715f
Ronald Record <github@ronrecord.com>
2023-02-22 20:53:05 +0000

    Get Java language server configuration working

--- conf/nvim/ftplugin/java.lua
+++ conf/nvim/ftplugin/java.lua
@@ -1,22 +1,10 @@
-local home = os.getenv('HOME')
 local jdtls = require('jdtls')
 local homebrew_install_dir = vim.g.homebrew_install_dir
-local config_os = "config_linux"
-local raw_os_name = io.popen('uname -s','r'):read('*l')
-if raw_os_name == "Darwin" then
-  config_os = "config_mac"
-end
 
 -- File types that signify a Java project's root directory. This will be
 -- used by eclipse to determine what constitutes a workspace
-local root_markers = {'gradlew', 'mvnw', '.git'}
-local root_dir = require('jdtls.setup').find_root(root_markers)
-
--- eclipse.jdt.ls stores project specific data within a folder. If you are working
--- with multiple different projects, each project must use a dedicated data directory.
--- This variable is used to configure eclipse to use the directory name of the
--- current project found using the root_marker as the folder for project specific data.
-local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+-- local root_markers = {'gradlew', 'mvnw', '.git'}
+-- local root_dir = require('jdtls.setup').find_root(root_markers)
 
 -- Helper function for creating keymaps
 function nnoremap(rhs, lhs, bufopts, desc)
@@ -59,52 +47,11 @@ local config = {
   -- The command that starts the language server
   -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
   cmd = {
-    homebrew_install_dir .. '/bin/java', -- or '/path/to/java17_or_newer/bin/java'
-
-    '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-    '-Dosgi.bundles.defaultStartLevel=4',
-    '-Declipse.product=org.eclipse.jdt.ls.core.product',
-    '-Dlog.protocol=true',
-    '-Dlog.level=ALL',
-    '-Xms1g',
-    '--add-modules=ALL-SYSTEM',
-    '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-    '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-
-    '-jar', homebrew_install_dir .. vim.fn.glob('/Cellar/jdtls/*/libexec/plugins/org.eclipse.equinox.launcher_*.jar'),
-
-    -- The configuration for jdtls is also placed where jdtls was installed.
-    '-configuration', homebrew_install_dir .. vim.fn.glob('/Cellar/jdtls/*/libexec/') .. config_os,
-
-    -- See `data directory configuration` section in the README
-    '-data', '/path/to/unique/per/project/workspace/folder'
+    homebrew_install_dir .. '/bin/jdtls'
   },
-
-  -- Here you can configure eclipse.jdt.ls specific settings
-  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
-  -- for a list of options
-  settings = {
-    java = {
-    }
-  },
-
-  -- Language server `initializationOptions`
-  -- You need to extend the `bundles` with paths to jar files
-  -- if you want to use additional eclipse.jdt.ls plugins.
-  --
-  -- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-  --
-  -- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-  init_options = {
-    bundles = {}
-  },
+  root_dir = vim.fs.dirname(vim.fs.find({'.gradlew', '.git', 'mvnw'}, { upward = true })[1]),
 }
 
--- Foo Bar
 -- This starts a new client & server,
 -- or attaches to an existing client & server depending on the `root_dir`.
-require('jdtls').start_or_attach(config)
-
--- Finally, start jdtls. This will run the language server using the configuration we specified,
--- setup the keymappings, and attach the LSP client to the current buffer
 jdtls.start_or_attach(config)
